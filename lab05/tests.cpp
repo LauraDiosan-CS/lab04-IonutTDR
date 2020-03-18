509189613ce0d18972328d3129ced4d01c2c7928
IonutTDR <62219246+IonutTDR@users.noreply.github.com>
2020-03-18 10:47:35 +0200

    Update tests.cpp

--- lab05/tests.cpp
+++ lab05/tests.cpp
@@ -7,10 +7,10 @@
 void test_GymExercise()
 {
     char* name = new char[10];
-    strcpy(name, "Ionut");
+    strcpy(name, "Ion");
     GymExercise g(name, 10, 30, 20);
     char* verif = g.getName();
-    assert(verif[0] == name[0]);
+    assert(verif[0] == name[0] && verif[1] == name[1] && verif[2] == name[2]);
     assert(g.getReps() == 30);
     assert(g.getSeries() == 10);
     assert(g.getWeight() == 20);
@@ -22,13 +22,13 @@ void test_Repo()
     Repo r;
     assert(r.getSize() == 0);
     char* name = new char[10];
-    strcpy(name,"Ionut");
+    strcpy(name,"Ion");
     GymExercise g(name, 10, 30, 20);
     r.addGymExercise(g);
     assert(r.getSize() == 1);
     GymExercise* gyms = r.getAll();
     char* verif = gyms[0].getName();
-    assert(verif[0] == name[0]);
+    assert(verif[0] == name[0] && verif[1] == name[1] && verif[2] == name[2]);
     assert (gyms[0].getReps() == 30);
     assert (gyms[0].getSeries() == 10);
 
